c93021df92c65e196a41944b967441cab115f65b
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-05-15 15:47:16 +0800

    進度
    1.完成基礎介面編輯

--- NotePad/MainWindow.xaml.cs
+++ NotePad/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO; // 要引用這個IO函式庫，才能作檔案處理
 
 namespace NotePad
 {
